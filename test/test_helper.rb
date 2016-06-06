988e1cf221ad51e2261caa183839d01beb08e9c5
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2016-06-06 08:05:50 -0600

    Complete setup for M3 assessment.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,29 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/autorun'
+require 'minitest/pride'
+require "capybara/rails"
+# require "test/unit"
+require "vcr"
+require "webmock"
+require "minitest-vcr"
+require "mocha/mini_test"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  VCR.configure do |config|
+    config.cassette_library_dir = 'test/cassettes'
+    config.hook_into :webmock
+    config.allow_http_connections_when_no_cassette = true
+  end
+  MinitestVcr::Spec.configure!
+
   # Add more helper methods to be used by all tests here...
+end
+
+class ActionDispatch::IntegrationTest
+  include Capybara::DSL
 end
