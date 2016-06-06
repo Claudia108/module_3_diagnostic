422f1236d3d20284bd91898c57e0eb3f5a0846d0
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2016-06-06 09:57:34 -0600

    Resolving errors with Faraday

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -1,16 +1,17 @@
 class NrelService
 
   def initialize
-    @connection = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?")
-    @connecton.headers[:Authorization] = ENV["nrel_api_key"]
+    @connection = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=#{ENV['nrel_api_key']}")
+    # @connecton.headers[:Authorization] = api_key, ENV["nrel_api_key"]
   end
 
   def parse(response)
     JSON.parse(response.body)
   end
 
-  def station_hash
-    parse(@connection.get "fuel_type=LPG,ELEC&zip=80206&limit=10")
+  def stations_hash
+    binding.pry
+    parse(@connection.get "?fuel_type=LPG,ELEC&zip=80206&limit=10")
   end
 
 end
